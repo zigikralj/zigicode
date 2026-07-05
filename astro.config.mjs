d7a9891f1162c70c2be17988e50a58d854f6c7a8
Nemanja Stanojevic <nemanja.stanojevic.kv@gmail.com>
2026-07-05 02:11:59 +0200

    Deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zigicode.com', // Change this to your custom domain or https://<username>.github.io
+  site: 'https://zigikralj.github.io', // Change this to your custom domain or https://<username>.github.io
   base: '/',                   // Change to '/<repo-name>' if deploying to a GitHub project repository (not custom domain)
   output: 'static',
 });
